63bb58a68890a5089f3d1b582d5f6b33c9c2126a
sojkaware <sojkaware@gmail.com>
2022-05-25 08:11:16 +0200

    Initial commit

--- app/src/main/java/com/example/weddingplayer/MainActivity.kt
+++ app/src/main/java/com/example/weddingplayer/MainActivity.kt
@@ -1,14 +1,53 @@
-package com.example.weddingplayer;
+package com.example.weddingplayer
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.weddingplayer.R
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
 
-public class MainActivity extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // create an instance of mediplayer for audio playback
+        val mediaPlayer: MediaPlayer = MediaPlayer.create(applicationContext, R.raw.way)
+
+        // register all the buttons using their appropriate IDs
+        val bPlay: Button = findViewById(R.id.playButton)
+        val bPause: Button = findViewById(R.id.pauseButton)
+        val bStop: Button = findViewById(R.id.stopButton)
+
+        // handle the start button to
+        // start the audio playback
+        bPlay.setOnClickListener {
+            // start method is used to start
+            // playing the audio file
+            mediaPlayer.start()
+        }
+
+        // handle the pause button to put the
+        // MediaPlayer instance at the Pause state
+        bPause.setOnClickListener {
+            // pause() method can be used to
+            // pause the mediaplyer instance
+            mediaPlayer.pause()
+        }
+
+        // handle the stop button to stop playing
+        // and prepare the mediaplayer instance
+        // for the next instance of play
+        bStop.setOnClickListener {
+            // stop() method is used to completely
+            // stop playing the mediaplayer instance
+            mediaPlayer.stop()
+
+            // after stopping the mediaplayer instance
+            // it is again need to be prepared
+            // for the next instance of playback
+            mediaPlayer.prepare()
+        }
     }
 }
